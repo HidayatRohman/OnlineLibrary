97bc83601d4f677b19d26b719d58097429695d1b
Aditya Dhani Sabang Saputra <adityasaputr7@gmail.com>
2015-12-28 10:42:17 +0700

    Perbaikan

--- app.js
+++ app.js
@@ -32,3 +32,65 @@ var UserSchema = new mongoose.Schema({
     phone: Number
 });
 var Users = mongoose.model('Users', UserSchema);
+//router
+app.get('/', routes.index);
+app.get('/users', function(req, res) {
+    Users.find({}, function(err, docs) {
+        res.render('users', {users: docs});
+    });
+});
+
+app.get('/users/new', function(req, res) {
+    res.render('users/new');
+});
+
+app.post('/users', function(req, res) {
+    var b = req.body;
+    new Users({
+        name: b.name,
+        email: b.email,
+        phone: b.phone
+    }).save(function(err, user) {
+            if(err) res.json(err);
+            res.redirect('/users/' + user.name);
+        })
+});
+
+app.param('name', function(req, res, next, name) {
+    Users.find({name:name}, function(err, docs) {
+        req.user = docs[0];
+        next();
+    });
+});
+//SHOW
+app.get('/users/:name', function(req, res) {
+    res.render('users/show', {user:req.user});
+});
+
+//EDIT
+app.get('/users/:name/edit', function(req, res){
+    res.render('users/edit', { user:req.user});
+});
+
+//UPDATE
+app.put('/users/:name', function(req, res) {
+    var b = req.body;
+        Users.update(
+        { name: req.params.name },
+        {name: b.name, email: b.email, phone: b.phone},
+            function(err) {
+                res.redirect('/users/' + b.name);
+            }
+    )
+});
+
+//DELETE
+app.delete('/users/:name', function(req, res) {
+    Users.remove({name:req.params.name}, function(err) {
+        res.redirect('/users');
+    })
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
