71056ab573c7ee01df06dc3060bbc147a6105fe3
Dody Adi Nugroho <didowadi2@gmail.com>
2015-12-30 09:54:51 +0700

    Memperbaiki Route

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+    res.send("respond with a resource");
+};
